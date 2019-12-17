cbddcb96627e0c868608dfc527412beb09e40e5c
DavideAntoninoGiorgio94 <davideantonino94@gmail.com>
2019-12-17 12:20:35 +0100

    modified CMakeLists

--- build/CMakeFiles/Makefile.cmake
+++ build/CMakeFiles/Makefile.cmake
@@ -43,5 +43,5 @@ set(CMAKE_MAKEFILE_PRODUCTS
 
 # Dependency information for all targets:
 set(CMAKE_DEPEND_INFO_FILES
-  "CMakeFiles/packetcapture.dir/DependInfo.cmake"
+  "CMakeFiles/sniffer.dir/DependInfo.cmake"
   )
